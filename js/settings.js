ee8dcdb6585425cec5170c7caec572e7769d2e22
Chris Hayes <chrishayesmu@gmail.com>
2015-09-24 01:40:49 -0700

    Add a functional settings menu that persists in localStorage; add support for hiding video chat and video comments

---
+++ js/settings.js
@@ -0,0 +1,144 @@
+/**
+ * This file contains functions for saving and loading application settings
+ * via HTML5 localStorage. Setting values are automatically converted to and
+ * from JSON when possible, so serializable objects can be stored.
+ */
+
+(function() {
+    // Default values for all settings. Only settings which have
+    // an entry in this object will be saved in localStorage.
+    var defaultSettings = {
+        HideImagesInChat: true,
+        HideVideoChat: false,
+        HideVideoComments: true
+    };
+
+    var $menu;
+
+    /**
+     * Applies the settings selected in the settings menu by saving them to localStorage.
+     */
+    function applySettings() {
+        var $inputElements = $menu.find("input");
+        var settings = {};
+
+        $.each($inputElements, function(index, element) {
+            var $element = $(element);
+            var inputType = $element.prop("type").toLowerCase();
+            var settingsName = $element.data("settings-name");
+            var settingsValue;
+
+            switch (inputType) {
+                case "checkbox":
+                    settingsValue = $element.prop("checked");
+                    break;
+                default:
+                    settingsValue = $element.val();
+                    break;
+            }
+
+            settings[settingsName] = settingsValue;
+        });
+
+        saveSettingsToLocalStorage(settings);
+        $menu.hide();
+
+        alert("You may need to refresh dubtrack to see changes take effect.");
+    };
+
+    /**
+     * Cancels the settings dialog and restores input elements to their last applied values.
+     */
+    function cancelSettings() {
+        var storedSettings = loadSettingsFromLocalStorage();
+        var $inputElements = $menu.find("input");
+
+        $inputElements.each(function(index, element) {
+            var $element = $(element);
+            var inputType = $element.prop("type").toLowerCase();
+
+            var settingsName = $element.data("settings-name");
+            var settingsValue = storedSettings[settingsName];
+
+            switch (inputType) {
+                case "checkbox":
+                    $element.prop("checked", !!settingsValue);
+                    break;
+                default:
+                    console.log("Didn't know how to handle input of type " + inputType);
+                    break;
+            }
+        });
+
+        $menu.hide();
+    }
+
+    /**
+     * Creates the settings menu and applies event handlers to its elements.
+     */
+    window.createSettingsMenu = function() {
+        var $menuTitle = $("<div style='cursor: pointer; position: absolute; top: 0.9em; right: 13em; z-index: 9999'>dubover settings</div>");
+
+        var menuCss = GM_getResourceText("SettingsMenuCss");
+        var menuHtml = GM_getResourceText("SettingsMenuTemplate");
+
+        injectCssInHead(menuCss);
+        $menu = $(menuHtml);
+
+        var $applyButton = $menu.find("#duboverSettingsMenuApplyButton");
+        var $cancelButton = $menu.find("#duboverSettingsMenuCancelButton");
+
+        $menuTitle.click(function() {
+            $menu.toggle();
+        });
+
+        $applyButton.click(applySettings);
+        $cancelButton.click(cancelSettings);
+
+        $(document.body).append($menu);
+        $(document.body).append($menuTitle);
+        cancelSettings(); // applies settings from localstorage
+    };
+
+    /**
+     * Loads the user's settings from localStorage. Settings are interpreted
+     * as JSON first and deserialized; if this fails, they are just treated as strings.
+     */
+    window.loadSettingsFromLocalStorage = function() {
+        var settings = {};
+
+        for (var key in defaultSettings) {
+            var value = localStorage.getItem(key);
+
+            if (value !== null) {
+                try {
+                    value = JSON.parse(value);
+                }
+                catch (e) {
+                }
+            }
+            else {
+                value = defaultSettings[key];
+            }
+
+            settings[key] = value;
+        }
+
+        return settings;
+    };
+
+    /**
+     * Saves the user's settings to localStorage. Setting values are serialized as JSON.
+     */
+    window.saveSettingsToLocalStorage = function(settings) {
+        for (var key in defaultSettings) {
+            var value = settings[key];
+
+            if (typeof value === "undefined") {
+                continue;
+            }
+
+            localStorage.setItem(key, JSON.stringify(value));
+        }
+    };
+})();
--- main.user.js
+++ main.user.js
@@ -1,49 +1,116 @@
 // ==UserScript==
 // @name       dubover
 // @namespace  https://github.com/chrishayesmu/dubover
-// @version    0.1.3
+// @version    0.2
 // @description Provides UI enhancements for dubtrack.fm
 // @match      https://www.dubtrack.fm/*
 // @copyright  2015+, Chris Hayes
 // @run-at     document-end
-// @require https://code.jquery.com/jquery-2.1.4.min
-// @require https://rawgit.com/chrishayesmu/dubover/master/settings.js
+// @require https://code.jquery.com/jquery-2.1.4.min.js
+// @require js/settings.js
+// @resource SettingsMenuCss css/settingsMenu.css
+// @resource SettingsMenuTemplate html/settingsMenu.html
 // @downloadURL https://rawgit.com/chrishayesmu/dubover/master/main.user.js
 // ==/UserScript==
 
-var settings = loadSettingsFromLocalStorage();
-initialize();
+(function() {
+    // Cached jQuery elements
+    var $toggleVideoChatElement;
+    var $videoCommentsElement;
 
-/**
- * Main entry point of the userscript.
- */
-function initialize() {
-	observeForImagesInChat();
-}
+    var imagesInChatObserver;
+    var settings;
 
-function observeForImagesInChat() {
-    var imagesInChatObserver = new MutationObserver(function(mutations) {
-        if (settings.HideImagesInChat) {
-            mutations.forEach(function(mutation) {
-                if (mutation.target && mutation.target.classList.contains("chat-main") && mutation.addedNodes.length > 0) {
-                    for (var i = 0; i < mutation.addedNodes.length; i++) {
-                        var $node = $(mutation.addedNodes[i]);
+    /**************************************************
+     * Functions which are exported to global namespace
+     **************************************************/
 
-                        // Find the added images (but only under a node with class autolink, or else
-                        // we'll accidentally replace the user's avatar as well)
-                        var $imgTags = $node.find("a.autolink > img");
+    /**
+     * Injects a CSS style element at the head of the document.
+     *
+     * @param {string} css - A valid CSS stylesheet to insert.
+     */
+    window.injectCssInHead = function(css) {
+        $("<style type='text/css'></style>").html(css).appendTo(document.head);
+    }
 
-                        $imgTags.each(function(index, item) {
-                            var $item = $(item);
-                            var src = $item.attr("src");
-                            $item.replaceWith(src);
-                        });
-                    }
-                }
-            });
+    /**************************************************
+     * Functions which are confined to this IIFE
+     **************************************************/
+
+    /**
+     * Main entry point of the userscript.
+     */
+    function initialize() {
+        console.log("Initializing dubover.");
+
+        settings = loadSettingsFromLocalStorage();
+        console.log("User's settings: ", settings);
+
+        $toggleVideoChatElement = $("#dubtrack-video-realtime .toggle_videos");
+        $videoCommentsElement = $("#room-comments");
+
+        createSettingsMenu();
+        observeForImagesInChat();
+        setDisplayOfVideoChat();
+        setDisplayOfVideoComments();
+
+        console.log("dubover initialization is complete.");
+    }
+
+    /**
+     * Toggles visibility of the video chat element.
+     */
+    function setDisplayOfVideoChat() {
+        $toggleVideoChatElement.toggle(!settings.HideVideoChat);
+    }
+
+    /**
+     * Toggles visibility of the video comments element.
+     */
+    function setDisplayOfVideoComments() {
+        $videoCommentsElement.toggle(!settings.HideVideoComments);
+    }
+
+    /**
+     * Sets up an observer which watches chat for incoming images,
+     * and replaces them with plain links if configured to do so.
+     */
+    function observeForImagesInChat() {
+        if (imagesInChatObserver != null) {
+            // We're already watching the chat
+            return;
         }
-    });
 
-    var chatDiv = document.getElementById("chat");
-    imagesInChatObserver.observe(chatDiv, { childList: true, subtree: true });
-}
+        imagesInChatObserver = new MutationObserver(function(mutations) {
+            if (settings.HideImagesInChat) {
+                mutations.forEach(function(mutation) {
+                    if (mutation.target && mutation.target.classList.contains("chat-main") && mutation.addedNodes.length > 0) {
+                        for (var i = 0; i < mutation.addedNodes.length; i++) {
+                            var $node = $(mutation.addedNodes[i]);
+
+                            // Find the added images (but only under a node with class autolink, or else
+                            // we'll accidentally replace the user's avatar as well)
+                            var $imgTags = $node.find("a.autolink > img");
+
+                            $imgTags.each(function(index, item) {
+                                var $item = $(item);
+                                var src = $item.attr("src");
+                                $item.replaceWith(src);
+                            });
+                        }
+                    }
+                });
+            }
+        });
+
+        var chatDiv = document.getElementById("chat");
+        imagesInChatObserver.observe(chatDiv, { childList: true, subtree: true });
+    }
+
+    /**************************************************
+     * Code which is executing immediately in the IIFE
+     **************************************************/
+
+    initialize();
+})();
